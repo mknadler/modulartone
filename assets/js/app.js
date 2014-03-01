72ae4fa4baa3f58cea35825f0fc346c4e69bb91c
Sophie Haskins <haskins.sophie@gmail.com>
2014-03-01 15:00:54 -0500

    js friendly keys

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,21 +14,21 @@ $("aside a").on("click", function (e) {
 });
 
 var scales = {
-	"minorsecond": "15:16",
-	"majorsecond": "8:9",
-	"minorthird": "5:6",
-	"majorthird": "4:5",
-	"perfectfourth": "3:4",
-	"augfourthdimfifth": "1:√2",
-	"perfectfifth": "2:3",
-	"minorsixth": "5:8",
-	"goldensection": "1:1.618",
-	"majorsixth": "3:5",
-	"minorseventh": "9:16",
-	"majorseventh": "8:15",
-	"octave": "1:2",
-	"majortenth": "2:5",
-	"majoreleventh": "3:8",
-	"majortwelfth": "1:3",
-	"doubleoctave": "1:4"
+	minorsecond: "15:16",
+	majorsecond: "8:9",
+	minorthird: "5:6",
+	majorthird: "4:5",
+	perfectfourth: "3:4",
+	augfourthdimfifth: "1:√2",
+	perfectfifth: "2:3",
+	minorsixth: "5:8",
+	goldensection: "1:1.618",
+	majorsixth: "3:5",
+	minorseventh: "9:16",
+	majorseventh: "8:15",
+	octave: "1:2",
+	majortenth: "2:5",
+	majoreleventh: "3:8",
+	majortwelfth: "1:3",
+	doubleoctave: "1:4"
 };
