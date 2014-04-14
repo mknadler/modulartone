a90ee11de06e4eab7742a092e179afdf43e4d607
Miriam Nadler <miriam.k.nadler@gmail.com>
2014-04-13 20:19:16 -0400

    rotating bolt (WIP)

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,10 +21,30 @@ var scales = {
 	doubleoctave: "1:4"
 };
 
+// I feel like this is maybe the most clumsy way to do this
+// but ... it works.
+var forsvg = {
+	minorsecond: 1,
+	majorsecond: 2,
+	minorthird: 3,
+	majorthird: 4,
+	perfectfourth: 5,
+	augfourthdimfifth: 6,
+	perfectfifth: 7,
+	minorsixth: 8,
+	goldensection: 9,
+	majorsixth: 10,
+	minorseventh: 11,
+	majorseventh: 12,
+	octave: 13,
+	majortenth: 14,
+	majoreleventh: 15,
+	majortwelfth: 16,
+	doubleoctave: 17
+};
 
 $("aside a.interval").on("click", function (e) {
     e.preventDefault();
-
     var next_interval = $(e.currentTarget).data("interval-class");
     if (current_interval) {
         $("body").removeClass(current_interval).addClass(next_interval);
@@ -34,9 +54,23 @@ $("aside a.interval").on("click", function (e) {
 
     $("section span").text(scales[next_interval]);
     ModularTone.sound.transitionTo(next_interval);
+
+    var bolt = $("svg");
+    var newLeft = $("svg span").css("left");
+
+    if (forsvg[current_interval] < forsvg[next_interval]) {
+    		bolt.transition({rotate: '-30deg'});
+    }
+    else if (forsvg[current_interval] > forsvg[next_interval]) {
+    		bolt.transition({rotate: '30deg'});
+    }
+    else {
+    	// don't turn
+    }
     current_interval = next_interval;
 });
 
 $('section').on('click', function (e) {
     ModularTone.sound.togglePlaying();
 });
+
